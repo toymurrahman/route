0abd1f5f7cc32946127196069bc09c4229006df2
toymurrahman <toymurrahman2@gmail.com>
2025-01-30 09:28:55 +0600

    Navigation added

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 
 const User = ({ user }) => {
     const{id , name, email , phone } =user;
+   
+    const userStyle = {
+        border: '2px solid yellow',
+        padding: '5px',
+        borderRadius: '20px',
+       
+    }
     return (
-        <div>
+        <div style={userStyle}>
             <h2>{name}</h2>
             <p>email: {email}</p>
             <p>phone: {phone}</p>
